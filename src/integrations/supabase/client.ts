042642c7bcdf5c546a713ac3e90eb7ff6ea1adc2
Manya Kalra <kalramanya212@gmail.com>
2026-05-21 22:27:51 +0530

    fixes

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://zhjuuvtowixfltodtqov.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoanV1dnRvd2l4Zmx0b2R0cW92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3OTY2NTgsImV4cCI6MjA2NzM3MjY1OH0.D2ts86Owxxj-R0Egu-ivl_0oh0PoI55Q6S2brPMfwls";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
